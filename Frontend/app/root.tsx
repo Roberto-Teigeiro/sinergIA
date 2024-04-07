63995c7f46620d98ffdb1ac78dc3c8e55e1cdccf
Roberto Teigeiro <A01643651@tec.mx>
2024-04-07 12:27:37 -0600

    first

--- Frontend/app/root.tsx
+++ Frontend/app/root.tsx
@@ -4,28 +4,44 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  LiveReload,
 } from "@remix-run/react";
+import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp, ClerkErrorBoundary } from "@clerk/remix";
+
+
+
+export const meta: MetaFunction = () => ([{
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+}]);
+ 
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
+
+
 
 import "./tailwind.css"
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+export const ErrorBoundary = ClerkErrorBoundary();
+
+function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
 }
 
-export default function App() {
-  return <Outlet />;
-}
+export default ClerkApp(App);
