a87a38df855274ea9c03f63af377defd6f8ea356
valele15 <136286504+valele15@users.noreply.github.com>
2024-04-25 19:41:25 -0600

    New Project conectada a base de datos

--- app/routes/api.submitNewProject.tsx
+++ app/routes/api.submitNewProject.tsx
@@ -1,5 +1,6 @@
-import { redirect } from "@remix-run/node";
+/*import { redirect } from "@remix-run/node";
 import { ActionFunctionArgs } from "@remix-run/node";
+import supabase from "utils/supabase";
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -16,6 +17,58 @@ export async function action({ request }: ActionFunctionArgs) {
     
 }
 return redirect("/");
+} */
+
+
+import { redirect } from "@remix-run/node";
+import { ActionFunction } from "@remix-run/node";
+import supabase from "utils/supabase";
+import { LoaderFunction } from "@remix-run/node";
+import { getAuth } from "@clerk/remix/ssr.server";
+import { v4 as uuidv4 } from 'uuid';
+
+export const action: ActionFunction = async (args) => {
+  const formData = await args.request.formData();
+  const { userId} = await getAuth(args);
+  console.log("User ID:", userId);
+
+  const keys = formData.keys();
+
+  for (const key of keys) {
+    const value = formData.get(key);
+    console.log({key});
+    console.log(value);
+  }
+
+  const nombre = formData.get("projectname");
+  const status = formData.get("projectstatus");
+  const current_resources = formData.get("currentresources");
+  const required_dept = formData.get("requireddepartments");
+  const required_users = formData.get("requireduserdepartment");
+  const project_requirements = formData.get("projectrequirements");
+  const objectives = formData.get("objectives");
+  
+  const Proyecto_id = uuidv4();
+  const Equipo_id = uuidv4();
+
+  const {error: proyectosError} = await supabase
+    .from('Proyectos')
+    .insert([{ Proyecto_id, nombre, status, current_resources, required_dept,
+    required_users, project_requirements, objectives }]);
+
+  if (proyectosError) {
+    console.error(proyectosError);
+    return redirect("/choosePath");
+  }
+
+  const {error: equiposError} = await supabase
+    .from('Equipos')
+    .insert({ Equipo_id, Proyecto_id, Usuario_id: userId });
+
+  if (equiposError) {
+    console.error(equiposError);
+    return redirect("/choosePath");
+  }
+
+  return redirect("/dashboard");
 }
-
-
---
+++ utils/supabase.ts
@@ -0,0 +1,15 @@
+/*import {createClient} from "@supabase/supabase-js";
+
+export default createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+);*/
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
+);
+
+export default supabase;
