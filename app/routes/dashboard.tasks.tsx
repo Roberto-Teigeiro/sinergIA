8e2e10897618147d1384a8c992cbebf3fc9438d9
valele15 <136286504+valele15@users.noreply.github.com>
2024-04-30 19:05:21 -0600

    Tasks traidos de base de datos a userId

--- app/routes/dashboard.tasks.tsx
+++ app/routes/dashboard.tasks.tsx
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { useOutletContext } from "@remix-run/react";
 import { LoaderFunction } from "@remix-run/node";
 import { getAuth } from "@clerk/remix/ssr.server";
@@ -7,58 +7,85 @@ import TaskPreview from "~/components/taskPreview";
 import TaskContainer from "~/components/taskContainer";
 import TaskModal from "~/components/taskModal";
 import Logo from "~/components/logo";
+import supabase from "utils/supabase";
+
+type TaskObject = {
+  id: string;
+  cardTitle: string;
+  description: string;
+  date: string;
+};
 
 export const loader: LoaderFunction = async (args) => {
-  const { userId, sessionId, getToken } = await getAuth(args);
+  const { userId } = await getAuth(args);
   if (!userId) {
     return redirect("/sign-in");
   }
-  console.log("rendering tasks! " + userId);
-  return userId;
+
+  const { data: tasksData, error } = await supabase
+    .from("Tareas")
+    .select("Tarea_id, task_name, due_date, description, status")
+    .eq("Usuario_id", userId);
+
+  if (error) {
+    console.error("Error:", error.message);
+    return redirect("/dashboard");
+  }
+
+  
+  const tasks: {
+    todo: TaskObject[];
+    inProgress: TaskObject[];
+    completed: TaskObject[];
+  } = {
+    todo: [],
+    inProgress: [],
+    completed: [],
+  };
+
+  tasksData?.forEach((task) => {
+    const { Tarea_id, task_name, due_date, description, status } = task;
+    const taskObject: TaskObject = {
+      id: Tarea_id,
+      cardTitle: task_name,
+      description: description,
+      date: due_date,
+    };
+
+    switch (status) {
+      case "To do":
+        tasks.todo.push(taskObject);
+        break;
+      case "In Progress":
+        tasks.inProgress.push(taskObject);
+        break;
+      case "Completed":
+        tasks.completed.push(taskObject);
+        break;
+      default:
+        break;
+    }
+  });
+
+  return { userId, tasks };
 };
 
-const tasks = [
-  {
-    id: "1",
-    cardTitle: "Task 1",
-    description: "Description 1",
-    date: "Due date",
-  },
-  {
-    id: "2",
-    cardTitle: "Task 2",
-    description: "Description 2",
-    date: "Due date",
-  },
-  {
-    id: "3",
-    cardTitle: "Task 3",
-    description: "Description 3",
-    date: "Due date",
-  },
-  {
-    id: "4",
-    cardTitle: "Task 4",
-    description: "Description 3",
-    date: "Due date",
-  },
-  {
-    id: "5",
-    cardTitle: "Task 5",
-    description: "Description 3",
-    date: "Due date",
-  },
-];
-
 export default function DashboardTask() {
-  //const { isLoaded, isSignedIn, user } = useUser();
+   const { userId, tasks } = useLoaderData() as {
+    userId: string;
+    tasks: {
+      todo: TaskObject[];
+      inProgress: TaskObject[];
+      completed: TaskObject[];
+    };
+  };
   return (
     <div className="ml-32">
       <Logo />
       <div className="flex z-0 ">
-        <TaskContainer tasks={tasks} typeofTask="To do" />
-        <TaskContainer tasks={tasks} typeofTask="In Progress" />
-        <TaskContainer tasks={tasks} typeofTask="Completed" />
+        <TaskContainer tasks={tasks.todo} typeofTask="To do" />
+        <TaskContainer tasks={tasks.inProgress} typeofTask="In Progress" />
+        <TaskContainer tasks={tasks.completed} typeofTask="Completed" />
       </div>
     </div>
   );
